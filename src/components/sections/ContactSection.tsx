dd31cb281c6c2bf2c0895a97934a57c1ca155402
markkoehler-cyber <kohlermark88@gmail.com>
2026-01-20 19:30:45 +0100

    Update ContactSection.tsx

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -51,7 +51,7 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-semibold text-foreground mb-1">Adresse</h4>
                   <p className="text-muted-foreground">
-                    Große Mohrengasse 38, 1020 Wien
+                    Harkortstraße 1, 1020 Wien
                   </p>
                 </div>
               </div>
@@ -63,10 +63,10 @@ export const ContactSection = () => {
                 <div>
                   <h4 className="font-semibold text-foreground mb-1">Telefon</h4>
                   <a 
-                    href="tel:+4930123456789" 
+                    href="tel:+06609037522" 
                     className="text-muted-foreground hover:text-primary transition-colors"
                   >
-                    +43664 5477777
+                    +06609037522
                   </a>
                 </div>
               </div>
@@ -100,15 +100,15 @@ export const ContactSection = () => {
             <div className="relative">
               <div className="aspect-square lg:aspect-[4/5] rounded-sm overflow-hidden border border-border">
   <iframe
-    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2658.4223682725105!2d16.384418131529028!3d48.217741038917524!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d07f2f17c1ad7%3A0x1264bd7ffac3ba32!2sFB%20Barbershop!5e0!3m2!1sen!2sat!4v1768417153210!5m2!1sen!2sat"
-    width="100%"
-    height="100%"
-    style={{ border: 0, filter: 'grayscale(100%) contrast(1.2)' }}
-    allowFullScreen
-    loading="lazy"
-    referrerPolicy="no-referrer-when-downgrade"
-    title="Standort FB Barbershop"
-  />
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1550.4295475941155!2d16.397543565275708!3d48.22215723839291!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476d071a5168bd13%3A0x6077c3c459533a7c!2sSwag%20Coiffure!5e0!3m2!1sde!2sat!4v1768933721436!5m2!1sde!2sat"
+  width="100%"
+  height="100%"
+  style={{ border: 0, filter: 'grayscale(100%) contrast(1.2)' }}
+  allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+  title="Standort Swag Coiffure"
+/>
 </div>
 
               {/* Overlay Card */}
